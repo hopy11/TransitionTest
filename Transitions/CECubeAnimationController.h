f47513a62387de106e78636b4d90e8751c7dfc11
hopy <hopy@live.cn>
2018-01-11 20:04:13 +0800

    first

---
+++ Transitions/CECubeAnimationController.h
@@ -0,0 +1,19 @@
+//
+//  CubeNavigationAnimator.h
+//  MovieQuiz
+//
+//  Created by Andrés Brun on 27/10/13.
+//  Copyright (c) 2013 Andrés Brun. All rights reserved.
+//
+
+#import "CEReversibleAnimationController.h"
+
+typedef enum {CubeAnimationWayHorizontal, CubeAnimationWayVertical} CubeAnimationWay;
+typedef enum {CubeAnimationTypeInverse, CubeAnimationTypeNormal} CubeAnimationType;
+
+@interface CECubeAnimationController : CEReversibleAnimationController
+
+@property (nonatomic, assign) CubeAnimationWay cubeAnimationWay;
+@property (nonatomic, assign) CubeAnimationType cubeAnimationType;
+
+@end
